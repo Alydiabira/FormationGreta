7160dbd471b6e0bd8408edcc75013e7297884e92
Aly <d-aly@outlook.fr>
2024-12-06 07:22:08 +0100

    Create UserController.php

---
+++ src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
+
+
+  
+
