47ad96946230902c3c7c0bb083c9efc4865d08f2
Aly <d-aly@outlook.fr>
2024-12-09 16:34:14 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,25 +1,33 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/user/new', name: 'user_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+
     {
-        
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'User created successfully!');
+            return $this->redirectToRoute('user_new');
+        }
+        return $this->render('user/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
-
-
-  
-
