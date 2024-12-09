8c4a13a915d14de89fd8687a9515bb521753a351
Aly <d-aly@outlook.fr>
2024-12-09 16:34:15 +0100

    Update UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,34 +3,29 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
 class UserType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-        
-            ->add('nom', TextType::class)
-            ->add('prenom')
-            ->add('mot_de_passe')
-            ->add('date_de_naissance', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('adresse')
-            ->add('ville')
-            ->add('code_postal')
-            ->add('phone_number')
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
-    }
+public function buildForm(FormBuilderInterface $builder, array $options): void
+{
+$builder
+->add('email', EmailType::class, [
+'label' => 'Email Address'
+])
+->add('name', TextType::class, [
+'label' => 'Full Name'
+]);
+}
+public function configureOptions(OptionsResolver $resolver): void
+{
+$resolver->setDefaults([
+'data_class' => User::class,
+]);
+}
 }
