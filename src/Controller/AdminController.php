877b15065365207e5f4622485aea6d9a15e52d59
Aly <d-aly@outlook.fr>
2024-12-09 16:34:13 +0100

    Update AdminController.php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function admin(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
