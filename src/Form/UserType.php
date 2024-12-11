3e5c3af7b34347d04b96aab398f9caefc49597f0
Aly <d-aly@outlook.fr>
2024-12-11 15:56:04 +0100

    Update UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,24 +8,49 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 
 class UserType extends AbstractType
 {
-public function buildForm(FormBuilderInterface $builder, array $options): void
-{
-$builder
-->add('email', EmailType::class, [
-'label' => 'Email Address'
-])
-->add('name', TextType::class, [
-'label' => 'Full Name'
-]);
-}
-public function configureOptions(OptionsResolver $resolver): void
-{
-$resolver->setDefaults([
-'data_class' => User::class,
-]);
-}
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email Address',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your email',
+                    'widget' => 'single_text',
+
+                ],
+
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Full Name',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your full name'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Full name cannot be empty',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your full name must be at least {{ limit }} characters long',
+                        'max' => 50,
+                        'maxMessage' => 'Your full name cannot be longer than {{ limit }} characters',
+                    ]),
+                ],
+            ]);
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
 }
