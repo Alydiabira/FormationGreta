4bfbe4e088855295b89f77c41c84d546123d6426
Aly <d-aly@outlook.fr>
2024-12-11 15:56:00 +0100

    Create AuthorController.php

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_admin_author_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function new(?Author $author, Request $request, EntityManagerInterface $manager): Response
+    {
+        $author ??= new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($author);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_admin_author_index');
+        }
+
+        return $this->render('admin/author/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+}
