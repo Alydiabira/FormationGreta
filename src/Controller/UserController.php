02ecf441ef55f7bfeb889eb9957ccdf2b9672254
Aly <d-aly@outlook.fr>
2024-12-12 11:31:38 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -30,4 +30,22 @@ class UserController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
+    public function notifications(): Response
+    {
+        // get the user information and notifications somehow
+        $userFirstName = '...';
+        $userNotifications = ['...', '...'];
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('user/notifications.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'user_first_name' => $userFirstName,
+            'notifications' => $userNotifications,
+        ]);
+    }
+
 }
