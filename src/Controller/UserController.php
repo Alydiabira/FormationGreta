e9371b86824c94f066be4ce3d6e2cf7763a52767
Aly <d-aly@outlook.fr>
2024-12-14 21:13:56 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,50 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-
 
 class UserController extends AbstractController
 {
-    #[Route('/user/new', name: 'user_new')]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('success', 'User created successfully!');
-            return $this->redirectToRoute('user_new');
-        }
-        return $this->render('user/new.html.twig', [
-            'form' => $form->createView(),
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
         ]);
     }
-
-
-    public function notifications(): Response
-    {
-        // get the user information and notifications somehow
-        $userFirstName = '...';
-        $userNotifications = ['...', '...'];
-
-        // the template path is the relative file path from `templates/`
-        return $this->render('user/notifications.html.twig', [
-            // this array defines the variables passed to the template,
-            // where the key is the variable name and the value is the variable value
-            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
-            'user_first_name' => $userFirstName,
-            'notifications' => $userNotifications,
-        ]);
-    }
-
 }
