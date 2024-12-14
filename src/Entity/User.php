4490a48f79408fdb118aa798f94e0c498dda98ce
Aly <d-aly@outlook.fr>
2024-12-14 21:14:00 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,9 +33,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $name = null;
-
     #[ORM\Column]
     private bool $isVerified = false;
 
@@ -112,18 +109,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function isVerified(): bool
