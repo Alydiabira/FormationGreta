05a6b2469a468571b58a75dd08b687e666492284
Aly <d-aly@outlook.fr>
2024-12-16 14:31:44 +0100

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
