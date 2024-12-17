0f732ad58e4b93496ada14cbfa428149aa4060e5
Aly <d-aly@outlook.fr>
2024-12-17 16:32:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,3 +11,13 @@ module.exports = {
     require('flowbite/plugin')
   ],
 }
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
